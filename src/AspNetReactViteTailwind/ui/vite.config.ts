f7d7e803d8503a248cf726a221456ab79c0d7591
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-02-19 23:52:28 +0100

    chore: add vite-plugin-mkcert
    pin version to 1.13.0 due to
    https://github.com/liuweiGL/vite-plugin-mkcert/issues/62

--- src/AspNetReactViteTailwind/ui/vite.config.ts
+++ src/AspNetReactViteTailwind/ui/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/app',
+  server: {
+    https: true,
+    port: 3001
+  },
+  plugins: [react(), mkcert()]
 })
